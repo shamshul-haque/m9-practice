b640a909a4bb9ac435a77729a3a049987ba2a57e
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-09-28 19:01:08 +0600

    firbase instaslled and setup configuration

---
+++ src/steps.js
@@ -0,0 +1,10 @@
+/* 
+
+1. visit: console.firebase.google.com
+2. create project (google analytics)
+3. register app (create configure)
+4. install: npm install firebase
+5. add config file to project
+6. Do not publish  or make firebase configuration(firebase.init.js) public by pushing to github
+
+*/
