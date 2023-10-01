37c715e606c0877c33da8193a6b69f317e5fc9e2
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-10-01 14:03:46 +0600

    valid prop types

--- auth-integration/src/components/PrivateRoute.jsx
+++ auth-integration/src/components/PrivateRoute.jsx
@@ -1,3 +1,4 @@
+import { PropTypes } from "prop-types";
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../provider/AuthProvider";
@@ -13,6 +14,10 @@ const PrivateRoute = ({ children }) => {
   }
 
   return <Navigate to="/login"></Navigate>;
+};
+
+PrivateRoute.propTypes = {
+  children: PropTypes.object,
 };
 
 export default PrivateRoute;
